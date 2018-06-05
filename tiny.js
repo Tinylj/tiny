21adab2b6824c535575d0e2d5b4bd7eb05c8dfba
lujin <luj@lhcis.com>
2018-06-05 17:31:16 +0800

    尝试实现观察者模式

--- tiny.js
+++ tiny.js
@@ -1,14 +1,4 @@
-var obj = {};
-Object.defineProperty(obj,"hello",{
-    get: function () {
-        console.log("get方法被调用了");
-    },
-    set: function () {
-        console.log("set方法被调用了");
-    }
-});
-obj.hello;
-obj.hello = "abc";
+
 
 var defineProperty = function(obj,key,val)
 {
@@ -29,10 +19,29 @@ var observe = function(obj)
         defineProperty(obj,key,obj[key]);
     });
 };
+//发布者函数
+var publisher = function()
+{
+    this.watchers = [];//订阅该发布者的订阅者
+};
+publisher.prototype = {
+  addWatcher:function(param){ //增加订阅者
+      this.watchers.add(param);
+  },
+  removeWatcher:function(param){ //删除订阅者
 
+  },
+  publish:function()
+  {
+    //发布功能
+  }
+};
+var watcher = function(node,vm)
+{
 
+};
 //编译节点
-var compile = function(node,vm)
+var compile = function(node,vm,publisher)
 {
     var reg = /\{\{(.*)\}\}/;
     if(node.nodeType === 1) //节点类型为元素
@@ -60,16 +69,18 @@ var compile = function(node,vm)
             node.nodeValue = vm.data[name];
         }
     }
+    var watcher = new watcher(node,vm)
+    publisher.addWatcher(watcher);
 };
 //劫持节点
-var nodeFragment = function(id,vm)
+var nodeFragment = function(id,vm,publisher)
 {
     var frag = document.createDocumentFragment(); // documentFragment 对象可以劫持 DOM结构。加入documentFragment的Node将自动从DOM中删除
     var app = document.getElementById(id);
     var child;
     while (child = app.firstChild)
     {
-        compile(child,vm);
+        compile(child,vm,publisher);
         frag.appendChild(child);
     }
     return frag;
@@ -80,6 +91,7 @@ var tiny = function(param)
     this.data = param.data;
     observe(this.data);
     var id = param.ele;
-    var dom = nodeFragment(id,this);
+    var dom = nodeFragment(id,this,mypublisher);
     document.getElementById(id).appendChild(dom);
+    var mypublisher = new publisher();
 };
